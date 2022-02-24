7a0e713c1fcd56343d0053c0b4a0f4e0509b879e
Soufiane Lakhfif <soufiane.lakhfif@gmail.com>
2022-02-24 09:20:39 +0100

    Fin de la première page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,40 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
+        const link = document.createElement( 'a' );
+        link.setAttribute('href', 'photographer.html?id='+id);
+
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const localisation = document.createElement( 'div' );
+        localisation.textContent = city + ', ' + country;
+        localisation.className = 'localisation';
+
+        const slogan = document.createElement( 'div' );
+        slogan.textContent = tagline;
+        slogan.className = 'slogan';
+
+        const pricing = document.createElement( 'div' );
+        pricing.textContent = price + '€/jour';
+        pricing.className = 'price';
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(localisation);
+        article.appendChild(slogan);
+        article.appendChild(pricing);
+        link.appendChild(article);
+        return (link);
     }
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,46 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+/**
+ * Fonction pour récupérer les photographes à partir d'un json
+ * @returns {Promise<{photographers: ({country: string, city: string, price: number, name: string, tagline: string, id: number, portrait: string}|{country: string, city: string, price: number, name: string, tagline: string, id: number, portrait: string})[]}>}
+ */
+async function getPhotographers() {
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    // Penser à remplacer par les données récupérées dans le json
+    let photographers = [];
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+    await fetch('http://localhost/fisheye/data/photographers.json')
+        .then(response => response.json())
+        .then(function (data){
+            photographers = data.photographers;
         });
-    };
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+    // et bien retourner le tableau photographers seulement une fois
+    return ({
+        photographers: [...photographers]
+    })
+
+}
+
+/**
+ * Récupère les valeurs des photographes via Json et les affiche
+ * @param photographers
+ * @returns {Promise<void>}
+ */
+async function displayData(photographers) {
+
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const {photographers} = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
